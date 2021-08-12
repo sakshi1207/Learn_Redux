45b44f13ab42a0d71bd85931cf10513cd463323e
sakshi <sakshianandk@gmail.com>
2021-08-12 17:51:45 +0530

    combineReducers in Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
-import './App.css';
+import React from "react";
+import { useSelector,useDispatch } from "react-redux";
+import {increment,decrement} from "./Actions";
 
 function App() {
+const counter = useSelector(state => state.counter)
+const isLogged = useSelector(state => state.isLogged)
+const dispatch = useDispatch();
+// we are using redux bcoz we dont hv to import everything in app file , we automatically call this will same time and space
+
   return (
-   <>
-   <h2>hello World</h2>
-   </>
+    <>
+      <div className="App">
+        <h1>Counter {counter}</h1>
+        <button onClick={() =>dispatch(increment())}>+</button>
+        <button onClick={() =>dispatch(decrement())}>-</button>
+        {isLogged ? <h3>for authorized person only</h3> : " "}
+
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,46 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
+import {createStore,combineReducers} from 'redux';//combine reducers in order to add multiple reducers in a single store
+import allReducers from "./Reducers/index"
+import {Provider} from "react-redux";
 
+const store=createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-//action
-const increment =() =>{
-  return{
-    type:"INCREMENT"
-  } 
-}
-const decrement =() =>{
-  return{
-    type:"DECREMENT"
-  } 
-}
-//reducer will tell us what is supposed to happen when we perform an action
-const counter =(state=0, action) =>{
-  switch(action.type){
-    case "INCREMENT":
-      return state+1
-      case "DECREMENT":
-      return state-1
-  }
-}
-//store is a place to store data as
-let store=createStore(counter)
-
-store.subscribe(() => console.log(store.getState()));
-//dispatcher
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
